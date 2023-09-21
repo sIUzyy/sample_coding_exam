1f90ab424745c696631cbfab073b188314db4c55
Justin Peligro <122611993+sIUzyy@users.noreply.github.com>
2023-09-21 20:54:42 +0800

    isPalindrome.js

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,3 +1,8 @@
+// 1. initialize a variable
+// 2. turn into array
+// 3. reverse it 
+// 4. back to string
+
 function isPalindrome (str){
     const pali = str.split('').reverse().join('') //reverse the string first
     return str === pali //check if the 'str' is equal to 'pali'. if yes return 'true' otherwise 'false'.
