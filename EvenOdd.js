8d8c1b533668b0809286d3a86c4c588295d095e0
Justin Peligro <justinpeligro.c04@gmail.com>
2024-12-23 16:47:18 +0800

    EvenOdd.js

--- EvenOdd.js
+++ EvenOdd.js
@@ -1,10 +1,11 @@
-function OddEven (n) {
-
-  //divide the 'n' to '2'. If theres no remainder meaning its 'even'. Otherwise, 'odd'.
-  if(n % 2 === 0 ){ // '0' will be placeholder for remainder.
-    return 'even'
-  }
-  return 'odd'
+function OddEven(n) {
+  // if the remainder of n is equal to 0 return even otherwise odd.
+  if (n % 2 === 0) return "even";
+  return "odd";
 }
 
-console.log(OddEven(0))
+// modulo return a remainder.
+// 10 / 2 is 5 with remainder of 0.
+// 11 / 2 is 5 with remainder of 5.
+console.log(OddEven(10)); // even. 10 % 2 = 0, 0 === 0? true. return even.
+console.log(OddEven(11)); // odd. 11 % 2 = 5, 5 === 0? false. return odd
