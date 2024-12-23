c307b616a4de5e878624785e87ec88e2f8760dc4
Justin Peligro <justinpeligro.c04@gmail.com>
2024-12-23 16:26:56 +0800

    reverseString.js

--- reverseString.js
+++ reverseString.js
@@ -1,26 +1,29 @@
-// 1. turn the string into array
-// 2. reverse it using 'reverse' function in javascript
-// 3. back to string
-
-//first appproach 
-function reverse (string) {
-    //first we split the string into an array, then reverse it using js function, after that we join them together.
-    return string.split('').reverse().join('')
-}
-const output = reverse('hello') //called the function and pass down 'hello' value in parameter
-console.log(output) //display the output
-
-
-// 1.initialized reversed
-// 2.loop through string
-
-//second approach
-function reverse (string) {
-    let reversed = '' //this is where the reverse string stored.
-    for(let c of string){ //'c' will represent the 'string' parameter.
-        reversed = c + reversed //each iteration the value of reversed will change. 'olleh'
-    }
-    console.log(reversed) //display the output
+// === METHOD: 01 ===
+// PROCEDURES: CONVERT TO ARRAY, REVERSE, BACK TO STRING.
+function reverseString(str) {
+  // split into array, reverse it, then join it back to string again.
+  return str.split("").reverse().join("");
 }
 
-reverse('hello') //called the function and pass down 'hello' value in parameter
+// === METHOD: 02 ===
+// PROCEDURES: INITIALIZE A VARIABLE, LOOP THROUGH STRING, RETURN THE INITIALIZE VARIABLE.
+function reverseStr(str) {
+  let reversed = ""; // it will represent the reversed string.
+
+  for (const c of str) {  // for loop through every single character of str.
+    
+    reversed = c + reversed; // update the reversed variable, backwards append. 
+  }
+
+  return reversed; // return the reversed string. (e.g. cat = t + a + c)
+}
+
+// === METHOD: 03 ===
+// PROCEDURE: CONVERT TO ARRAY, USED REDUCE METHOD, INITIAL VALUE IS "".
+function reversedStr(str) {
+    
+  // convert to array - Array.from allow us to pass in an iterable object like the split("").
+  // reduce method will take an array of item then reduce it down to single result
+  // and the initial value or stating value is ""
+  return Array.from(str).reduce((reversed, c) => c + reversed, ""); // cat = t + a + c
+}
